b859b84f6d48ac76eefbb84ab58285105e2e5e6a
enxa <cezary.walczak.pl@gmail.com>
2022-06-27 18:04:21 +0200

    Add modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let isOpen: boolean = true
+</script>
+
+<template>
+  {#if isOpen}
+    <section class="modal">
+      <div>
+        <slot />
+      </div>
+    </section>
+  {/if}
+</template>
+
+<style lang="scss">
+  .modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(23, 26, 31, .75);
+    display: grid;
+    place-items: center;
+    div {
+      background: var(--color-alabaster);
+      padding: 2rem;
+      border-radius: 1rem;
+    }
+  }
+</style>
